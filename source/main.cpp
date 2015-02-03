5874a9bec6f9e7f10df65e7dae699e6e8db8a50b
IggyZuk <IgnatusZuk@gmail.com>
2015-02-03 19:36:51 +0000

    Vector and Matrix transformations added

--- source/main.cpp
+++ source/main.cpp
@@ -2,19 +2,227 @@
 #include <SFML\Window.hpp>
 #include <iostream>
 #include <assert.h>
+#include <cstring>
 #include <stdlib.h>
 #include <math.h>
 #define PI 3.14159265
+
+template <typename T>
+void LOG(const T& value) {
+    std::cout << value << std::endl;
+}
+
+template <typename U, typename... T>
+void LOG(const U& head, const T&... tail) {
+    std::cout << head << "; ";
+    LOG(tail...);
+}
 
 float random() {
     return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 }
 
-class Vertex {
+class Vector4 {
 public:
-    Vertex(float x, float y) {
+    Vector4(float x, float y, float z, float w = 1) {
         this->x = x;
         this->y = y;
+        this->z = z;
+        this->w = w;
+    }
+    float length() const {
+        return (float)sqrt(x*x + y*y + z*z + w*w);
+    }
+    Vector4 normalize() {
+        float len = length();
+        if(len != 0) {
+            x /= len;
+            y /= len;
+            z /= len;
+            w /= len;
+        }
+        return *this;
+    }
+    float dot(const Vector4& v) const {
+        return x * v.x + y * v.y + z * v.z + w * v.w;
+    }
+    Vector4 cross(const Vector4& v) {
+        float xx = y * v.z - z * v.y;
+        float yy = z * v.x - x * v.z;
+        float zz = x * v.y - y * v.x;
+        return Vector4(xx, yy, zz, 0);
+    }
+    Vector4 lerp(const Vector4& dest, float factor) {
+        return ((dest - *this) * factor) + *this;
+    }
+    Vector4 operator+(const Vector4& v) const {
+        return Vector4(x + v.x, y + v.y, z + v.z, w + v.w);
+    }
+    Vector4 operator-(const Vector4& v) const {
+        return Vector4(x - v.x, y - v.y, z - v.z, w - v.w);
+    }
+    Vector4 operator*(const int factor) const {
+        return Vector4(x * factor, y * factor, z * factor, w * factor);
+    }
+    Vector4 operator/(const int factor) const {
+        assert(factor != 0);
+        return Vector4(x / factor, y / factor, z / factor, w / factor);
+    }
+    float x;
+    float y;
+    float z;
+    float w;
+};
+std::ostream& operator<<(std::ostream& out, const Vector4& rhs) {
+    out << "(" << rhs.x << ", " << rhs.y << ", " << rhs.z << ")";
+    return out;
+}
+
+class Matrix4 {
+public:
+    Matrix4() {
+        identity();
+    }
+    void identity() {
+        for (unsigned int i = 0; i < 4; ++i) {
+            for (unsigned int j = 0; j < 4; ++j) {
+                matrix[j][i] = (i == j ? 1.0f : 0.0f);
+            }
+        }
+    }
+    void viewport(unsigned short width, unsigned short height) {
+        identity();
+        unsigned short halfWidth = width / 2;
+        unsigned short halfHeight = height / 2;
+        matrix[0][0] = halfWidth;
+        matrix[1][1] = -halfHeight;
+        matrix[3][0] = halfWidth;
+        matrix[3][1] = halfHeight;
+    }
+    void perspective(float fov, float aspect, float znear, float zfar) {
+        float xymax = znear * tan((float)(fov * PI / 180.0f));
+        float ymin = -xymax;
+        float xmin = -xymax;
+
+        float width = xymax - xmin;
+        float height = xymax - ymin;
+
+        float depth = zfar - znear;
+        float q = -(zfar + znear) / depth;
+        float qn = -2 * (zfar * znear) / depth;
+
+        float w = 2 * znear / width;
+        w = w / aspect;
+        float h = 2 * znear / height;
+
+        matrix[0][0] = w;
+        matrix[0][1] = 0.0f;
+        matrix[0][2] = 0.0f;
+        matrix[0][3] = 0.0f;
+        matrix[1][0] = 0.0f;
+        matrix[1][1] = h;
+        matrix[1][2] = 0.0f;
+        matrix[1][3] = 0.0f;
+        matrix[2][0] = 0.0f;
+        matrix[2][1] = 0.0f;
+        matrix[2][2] = q;
+        matrix[2][3] = -1.0f;
+        matrix[3][0] = 0.0f;
+        matrix[3][1] = 0.0f;
+        matrix[3][2] = qn;
+        matrix[3][3] = 0.0f;
+    }
+    Matrix4 operator*(const Matrix4& mat) {
+        Matrix4 m;
+        for (unsigned int i = 0; i < 4; ++i) {
+            for (unsigned int j = 0; j < 4; ++j) {
+                m[i][j] = matrix[0][j] * mat.matrix[i][0] +
+                          matrix[1][j] * mat.matrix[i][1] +
+                          matrix[2][j] * mat.matrix[i][2] +
+                          matrix[3][j] * mat.matrix[i][3];
+            }
+        }
+        return m;
+    }
+    Vector4 operator*(const Vector4& vec) const {
+        float a[4];
+        for (unsigned int i = 0; i < 4; ++i) {
+            a[i] = matrix[0][i] * vec.x +
+                   matrix[1][i] * vec.y +
+                   matrix[2][i] * vec.z +
+                   matrix[3][i] * vec.w;
+        }
+        return Vector4(a[0], a[1], a[2], a[3]);
+    }
+    float* operator[](int a) {
+        return matrix[a];
+    }
+    void translate(float x, float y, float z) {
+        Matrix4 m;
+        m[3][0] = x;
+        m[3][1] = y;
+        m[3][2] = z;
+        *this = *this * m;
+    }
+    void rotateX(float angle) {
+        Matrix4 m;
+        float radian = (angle * ((float)PI / 180.0f));
+        float sinus = sin(radian);
+        float cosinus = cos(radian);
+        m[1][1] = cosinus;
+        m[2][2] = cosinus;
+        m[1][2] = sinus;
+        m[2][1] = -sinus;
+        *this = *this * m;
+    }
+
+    void rotateY(float angle) {
+        Matrix4 m;
+        float radian = (angle * ((float)PI / 180.0f));
+        float sinus = sin(radian);
+        float cosinus = cos(radian);
+        m[0][0] = cosinus;
+        m[2][2] = cosinus;
+        m[0][2] = -sinus;
+        m[2][0] = sinus;
+        *this = *this * m;
+    }
+
+    void rotateZ(float angle) {
+        Matrix4 m;
+        float radian = (angle * ((float)PI / 180.0f));
+        float sinus = sin(radian);
+        float cosinus = cos(radian);
+        m[0][0] = cosinus;
+        m[1][1] = cosinus;
+        m[0][1] = sinus;
+        m[1][0] = -sinus;
+        *this = *this * m;
+    }
+    void scale(float x, float y, float z) {
+        Matrix4 m;
+        m[0][0] = x;
+        m[1][1] = y;
+        m[2][2] = z;
+        *this = *this * m;
+    }
+private:
+    float matrix[4][4];
+};
+
+class Vertex {
+public:
+    Vertex(Vector4 vec) :
+        position(vec),
+        x(position.x),
+        y(position.y),
+        z(position.z) {
+    }
+    Vertex(const Vertex& rhs) :
+                position(rhs.position),
+                x(position.x),
+                y(position.y),
+                z(position.z) {
     }
     float triangleAreaTimesTwo(Vertex b, Vertex c) {
         float x1 = b.x - x;
@@ -25,11 +233,31 @@ public:
 
         return (x1 * y2 - x2 * y1);
     }
-    static Vertex Random(float xRange, float yRange) {
-        return Vertex(random() * xRange, random() * yRange);
+    static Vertex Random() {
+        Vector4 v((random() - 0.5f) * 2.0f, (random() - 0.5f) * 2.0f, 1.0f, 1.0f);
+        return Vertex(v);
     }
-    float x;
-    float y;
+    Vertex transform(Matrix4& mat) {
+        return Vertex(mat * position);
+    }
+    Vertex perspectiveDivide() const {
+        return Vertex(Vector4(x/position.w, y/position.w, z/position.w, position.w));
+    }
+    Vertex& operator=(const Vertex& vec) {
+        position = vec.position;
+        x = position.x;
+        y = position.y;
+        z = position.z;
+        return *this;
+    }
+
+private:
+    Vector4 position;
+
+public:
+    float& x;
+    float& y;
+    float& z;
 };
 
 std::ostream& operator<<(std::ostream& out, const Vertex& rhs) {
@@ -45,14 +273,20 @@ public:
         this->b = b;
         this->a = a;
     }
+    Color(unsigned int hex) {
+        this->r = (hex >> 24) & 0xFF;
+        this->g = (hex >> 16) & 0xFF;
+        this->b = (hex >> 8 ) & 0xFF;
+        this->a = (hex      ) & 0xFF;
+    }
     static Color Random() {
-        return Color(random()*255, random()*255, random()*255, 255);
+        return Color(random()*0xFF, random()*0xFF, random()*0xFF, 0xFF);
     }
     static Color Grey() {
-        return Color(24, 24, 24, 255);
+        return Color(0x151515FF);
     }
     static Color White() {
-        return Color(255, 255, 255, 255);
+        return Color(0xFFFFFFFF);
     }
 
     unsigned char r;
@@ -80,7 +314,7 @@ public:
     }
     void setPixel(unsigned int x, unsigned int y, Color color) {
         int index = (x + y * width) * 4;
-        if(index >= width * height * 4) return;
+        if(index < 0 || index >= width * height * 4) return;
 
         pixels[index]     = color.r; // R
         pixels[index + 1] = color.g; // G
@@ -205,21 +439,26 @@ public:
         scanBuffer[y * 2 + 1] = xMax;
     }
     void fillShape(unsigned int yMin, unsigned int yMax) {
-        assert(yMin >= 0 && yMax <= height);
+        if(yMin < 0 || yMax >= height) return;
+
         Color randomColor= Color::Random();
         for(unsigned int j = yMin; j < yMax; ++j) {
             unsigned int xMin = scanBuffer[j * 2];
             unsigned int xMax = scanBuffer[j * 2 + 1];
 
+            if(!xMin || !xMax) return;
             for(unsigned int i = xMin; i < xMax; ++i) {
                 setPixel(i, j, randomColor);
             }
         }
     }
     void fillTriangle(Vertex v1, Vertex v2, Vertex v3) {
-        Vertex minYVert = v1;
-        Vertex midYVert = v2;
-        Vertex maxYVert = v3;
+        Matrix4 screenspace;
+        screenspace.viewport(width, height);
+
+        Vertex minYVert = v1.transform(screenspace).perspectiveDivide();
+        Vertex midYVert = v2.transform(screenspace).perspectiveDivide();
+        Vertex maxYVert = v3.transform(screenspace).perspectiveDivide();
 
         if(maxYVert.y < midYVert.y) {
             Vertex temp = maxYVert;
@@ -283,20 +522,35 @@ int main() {
     Stars3D game(1024, 64.0f, 20.0f);
 
     sf::Clock clock;
-    float counter = 0.5f;
+    float counter = 0.0f;
+
+    Matrix4 projection;
+    projection.perspective(60.0f, 800.0f/600.0f, 0.1f, 1000.0f);
+
+    Vertex v1 = Vertex(Vector4(-1.0f, -1.0f, 0.0f));
+    Vertex v2 = Vertex(Vector4(0.0f, 1.0f, 0.0f));
+    Vertex v3 = Vertex(Vector4(1.0f, -1.0f, 0.0f));
 
     while(display.isOpen()) {
 
         float dt = clock.restart().asSeconds();
-        counter -= dt;
+        counter += dt;
 
         game.render(context, dt);
 
-        Vertex v1 = Vertex::Random(800.0f, 600.0f);
-        Vertex v2 = Vertex::Random(800.0f, 600.0f);
-        Vertex v3 = Vertex::Random(800.0f, 600.0f);
+        Matrix4 transform;
+        transform.translate(0.0f, 0.0f, -5.0f);
+        transform.rotateX(counter * 100.0f);
+        transform.rotateY(counter * 75.0f);
+        transform.rotateZ(counter * 50.0f);
+        transform.scale(2.0f, 2.0f, 2.0f);
 
-        context.fillTriangle(v1,v2,v3);
+        Matrix4 mvp = projection * transform;
+
+        context.fillTriangle(
+                v1.transform(mvp),
+                v2.transform(mvp),
+                v3.transform(mvp));
 
         display.draw();
     }
